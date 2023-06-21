8d08339ded54aec72f15e013606a98dbd1dca250
Cheikh Saadbouh <cheikhsoftdev@gmail.com>
2023-06-21 01:35:36 -0500

    refactor: remove comments

--- API/utils/requestValidator.util.js
+++ API/utils/requestValidator.util.js
@@ -2,29 +2,6 @@ const jwt = require("jsonwebtoken");
 const responseUtil = require("./response.util");
 const util = require("util");
 const _jwtVerify = util.promisify(jwt.verify);
-// const isAuthorized = (req, res, next) => {
-//     const { authorization } = req.headers;
-
-//     if (!authorization || !authorization.startsWith("bearer ")) {
-//       return res.status(401).json({ error: "Unauthorized" });
-//     }
-
-//     const token = authorization.slice(7);
-
-//     try {
-//       const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-
-//       const currentTime = Math.floor(Date.now() / 1000);
-//       if (decodedToken.exp > currentTime) {
-//         next();
-//       } else {
-//         res.status(401).json({ error: "Token expired" });
-//       }
-//     } catch (err) {
-//       res.status(401).json({ error: "Unauthorized" });
-//     }
-//   };
-
 
 
 const isAuthorized = function(req, res, next) {
--- app.js
+++ app.js
@@ -8,12 +8,12 @@ const routes = require("./API/Routes");
 
 const app = express();
 
-// app.use(process.env.API, function(req, res, next) {
-//   res.header(process.env.ACCESS_CONTROL_ALLOW_ORIGIN, process.env.ALLOW_ORIGIN);
-//    res.header(process.env.ACCESS_CONTROL_ALLOW_HEADERS, process.env.ALLOW_HEADERS);
-//    res.header(process.env.ACCESS_CONTROL_ALLOW_METHODS,process.env.ALLOW_METHODS);
-//    next()
-// });
+app.use(process.env.API, function(req, res, next) {
+  res.header(process.env.ACCESS_CONTROL_ALLOW_ORIGIN, process.env.ALLOW_ORIGIN);
+   res.header(process.env.ACCESS_CONTROL_ALLOW_HEADERS, process.env.ALLOW_HEADERS);
+   res.header(process.env.ACCESS_CONTROL_ALLOW_METHODS,process.env.ALLOW_METHODS);
+   next()
+});
 
 app.use(bodyParser.json({ limit: process.env.SIZE_LIMIT })); // animal and park picture are being converted to base64 and its size excced the default http size limit
 
